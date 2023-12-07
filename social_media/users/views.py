70b9d209eeefbf5128baa5a946615614466fd68e
ShubhenduADutta <shubhendu@flexli.in>
2023-12-07 17:58:24 +0530

    Create login view

--- social_media/users/views.py
+++ social_media/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .forms import LoginForm
 
 # Create your views here.
+
+
+def user_login(request):
+    form = LoginForm()
+    return render(request, 'users/login.html', context={'form': form})
