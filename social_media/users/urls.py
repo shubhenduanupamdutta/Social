13a10503c0defce7de7a3b54420f16ac7e8159a3
ShubhenduADutta <shubhendu@flexli.in>
2023-12-07 19:11:45 +0530

    Add password reset functionality.
    Create a url for password reset
    Put that url on login page
    create password reset form template
    create url and template for password reset done
    create url and template for password reset confirm
    create url and template for password reset complete

--- social_media/users/urls.py
+++ social_media/users/urls.py
@@ -14,5 +14,17 @@ urlpatterns = [
     path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(
         template_name="users/password_change_done.html"),
         name='password_change_done'),
-
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name="users/password_reset_form.html"),
+        name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+        template_name="users/password_reset_done.html"),
+        name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="users/password_reset_confirm.html"),
+         name='password_reset_confirm'),
+    path('reset_complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name="users/password_reset_complete.html"),
+        name='password_reset_complete')
 ]
