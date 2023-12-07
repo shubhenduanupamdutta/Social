1dffc9cb7e9d8a75b817c69c640c98b26a621a1c
ShubhenduADutta <shubhendu@flexli.in>
2023-12-07 18:17:06 +0530

    Modify user_login to handle post request

--- social_media/users/views.py
+++ social_media/users/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import LoginForm
 
@@ -5,5 +7,19 @@ from .forms import LoginForm
 
 
 def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            # process form data
+            data = form.cleaned_data
+            user = authenticate(
+                request, username=data['username'], password=data['password'])
+            if user:
+                login(request, user)
+                return HttpResponse(
+                    f"Welcome {user.username}! you are logged in.")  # type: ignore # noqa
+            else:
+                return HttpResponse("Invalid login details supplied.")
+
     form = LoginForm()
     return render(request, 'users/login.html', context={'form': form})
