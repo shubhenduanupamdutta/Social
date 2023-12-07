1ba233dbbda2a4aa9827e4fdbcbd7e16d7e69250
ShubhenduADutta <shubhendu@flexli.in>
2023-12-07 18:52:30 +0530

    Add change password functionality.

--- social_media/users/urls.py
+++ social_media/users/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(
         template_name="users/logout.html"), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(
+        template_name="users/password_change_form.html"),
+        name='password_change'),
+    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name="users/password_change_done.html"),
+        name='password_change_done'),
+
 ]
