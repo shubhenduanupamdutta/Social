4d75f672a69762fbc18be35870a8856b844193cf
ShubhenduADutta <shubhendu@flexli.in>
2023-12-07 18:30:11 +0530

    Create a logout url and template.

--- social_media/users/urls.py
+++ social_media/users/urls.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name="users/logout.html"), name='logout'),
 ]
