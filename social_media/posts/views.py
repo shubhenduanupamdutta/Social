656347f796dc87627a4353840138a81a8b55c74f
ShubhenduADutta <shubhendu@flexli.in>
2023-12-08 17:17:21 +0530

    Create view to handle creation of a post

--- social_media/posts/views.py
+++ social_media/posts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import PostCreateForm
+
 
 # Create your views here.
+def post_create(request):
+    if request.method == "POST":
+        form = PostCreateForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.user = request.user
+            new_post.save()
+
+        return redirect("index")
+
+    form = PostCreateForm()
+    return render(request, "posts/post_create.html", {"form": form})
