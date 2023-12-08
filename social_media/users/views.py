7163155d9347d500fc8e6ee0b1c9d5d0ad9ce09c
ShubhenduADutta <shubhendu@flexli.in>
2023-12-08 16:33:15 +0530

    Create view for editing User and Profile details

--- social_media/users/views.py
+++ social_media/users/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
-from .forms import LoginForm, UserRegisterForm
+from django.shortcuts import redirect, render
+from .forms import LoginForm, UserRegisterForm, UserEditForm, ProfileEditForm
 from .models import Profile
 
 # Create your views here.
@@ -45,3 +45,25 @@ def register(request):
 
     form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+        return redirect('index')
+
+    user_form = UserEditForm(instance=request.user)
+    profile_form = ProfileEditForm(instance=request.user.profile)
+
+    context = {"user_form": user_form, "profile_form": profile_form}
+    return render(request, 'users/edit.html', context=context)
