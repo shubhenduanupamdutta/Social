6cad38e8792c93b313f7db82152c021fdc420716
ShubhenduADutta <shubhendu@flexli.in>
2023-12-08 16:57:15 +0530

    Create Posts model

--- social_media/posts/models.py
+++ social_media/posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Posts(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="images/%Y/%m/%d/")
+    caption = models.TextField(blank=True)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    
