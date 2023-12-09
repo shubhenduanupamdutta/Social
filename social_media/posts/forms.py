54e43346a69ef17de407bcaf184a35274392a50c
ShubhenduADutta <shubhendu@flexli.in>
2023-12-09 12:37:13 +0530

    Add some tailwind classes to input fields in forms

--- social_media/posts/forms.py
+++ social_media/posts/forms.py
@@ -6,3 +6,10 @@ class PostCreateForm(forms.ModelForm):
     class Meta:
         model = Posts
         fields = ("title", "image", "caption")
+
+    def __init__(self, *args, **kwargs):
+        super(PostCreateForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'w-full focus:outline-none'
+            })
--- social_media/users/forms.py
+++ social_media/users/forms.py
@@ -8,27 +8,60 @@ class UserEditForm(forms.ModelForm):
         model = User
         fields = ('first_name', 'last_name', 'email')
 
+    def __init__(self, *args, **kwargs):
+        super(UserEditForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'w-full focus:outline-none'
+            })
+
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('photo', )
 
+    def __init__(self, *args, **kwargs):
+        super(ProfileEditForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'w-full focus:outline-none'
+            })
+
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100)
+    username = forms.CharField(
+        label='Username',
+        max_length=100,
+        widget=forms.TextInput)
     password = forms.CharField(
-        label='Password', widget=forms.PasswordInput, max_length=100)
+        label='Password',
+        widget=forms.PasswordInput,
+        max_length=100)
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'w-full focus:outline-none'
+            })
 
 
 class UserRegisterForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(
-        widget=forms.PasswordInput, label="Confirm Password")
+    password2 = forms.CharField(widget=forms.PasswordInput(),
+                                label="Confirm Password")
 
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name')
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'w-full focus:outline-none'
+            })
 
     def check_password(self):
         if self.cleaned_data['password'] != self.cleaned_data['password2']:
