2528bb3e16e8ca7664c46d5b7bd8265f9a1aa51a
ShubhenduADutta <shubhendu@flexli.in>
2023-12-12 16:35:03 +0530

    Add a url for implementing likes

--- social_media/posts/urls.py
+++ social_media/posts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("feed/", views.feed, name="feed"),
     path("create/", views.post_create, name="create"),
+    path("like/", views.like_post, name="like"),
 ]
